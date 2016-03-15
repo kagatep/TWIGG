3b4c0e3e5ee3124cace859c530f5fd365d88dcf0
Krisha Agatep <krishaagatep@yahoo.com>
2016-03-14 20:09:49 -0700

    Reversing stuff we love

--- wp-content/themes/sage-master/assets/scripts/stuffWeLove.js
+++ wp-content/themes/sage-master/assets/scripts/stuffWeLove.js
@@ -1,39 +1,4 @@
 $(document).ready(function() {
-//https://docs.google.com/spreadsheets/d/1_5-Gsf_mss7iCN2uchFxpiNntjio3ZIxlarCuQSv3Ys/pubhtml?gid=0&single=true
-  var spreadsheetID = "1_5-Gsf_mss7iCN2uchFxpiNntjio3ZIxlarCuQSv3Ys";
- 
- // Make sure it is public or set to Anyone with link can view 
- var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
- 
- $.getJSON(url, function(data) {
- 
-
-  var entry = data.feed.entry;
- 
-  $(entry).each(function(){
-    // Column names are name, age, etc.
-    //$('.results').prepend('<h2>'+this.gsx$name.$t+'</h2><p>'+this.gsx$age.$t+'</p>');
-    var articles = '[{' + this.gsx$articles.$t.replace(/name:/g,'"name":')
-                                             .replace(/link:/g,'"link":')
-                                             .replace(/" "name/g,'"},{"name') + 
-                    '}]';
-    var articlesArray = $.parseJSON(articles);
-    var articlesString = '';
-    for(var i = 0; i < articlesArray.length; i++) {
-      articlesString  =  articlesString + '<li><a href="' + articlesArray[i].link + '" target="_blank">' +
-                         articlesArray[i].name + '</a></li>';
-    }
-  
-    $('.stuff-we-love').find('.main').prepend(
-        '<h3>' + this.gsx$date.$t + '</h3>' +
-        '<ul>' + articlesString + '</ul>' + 
-        '<p><h4>Videos:</h4> ' + this.gsx$videos.$t + '</p>' +
-        '<p><h4>Dance Party Tune:</h4> ' + this.gsx$song.$t + '</p>' +
-        '<p><h4>Quote:</h4> ' + this.gsx$quote.$t + '</p>' + '<hr />'
-      );
-  });
- 
- });
 
   $('.stuff').click(function() {
 
