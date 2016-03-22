0a62ab2bc9c7219b4c09f85b8a8a1131fab6e973
Krisha Agatep <krishaagatep@yahoo.com>
2016-03-21 19:55:26 -0700

    Initial new footer setup

--- wp-content/themes/sage-master/templates/footer.php
+++ wp-content/themes/sage-master/templates/footer.php
@@ -1,11 +1,112 @@
+<div class="container subscribe">
+<div class="row">
+	<div class="subscribeLink leftBorder col-xs-12 col-sm-6 col-md-6">
+	    <h2 class="script"><a href="http://eepurl.com/bd6_Rb" target="_blank">Subscribe</a></h2>
+		<h4>to our monthly newsletter!</h4>
+
+		<div class="mailchimpForm">
+		<!-- Begin MailChimp Signup Form -->
+		<link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+		<style type="text/css">
+			#mc_embed_signup .mc-field-group {
+				width: 60%;
+				float: left;
+			}
+			.submit-mce {
+				float: left;
+				margin-top: 6px;
+				margin-left: 10px;
+			}
+			#mc_embed_signup .mc-field-group .asterisk {
+				top: 0;
+			}
+			#mc_embed_signup_scroll input[type="text"] {
+				padding: 5px 10px;
+			    font-size: 14px;
+			    line-height: 1.428571429;
+			    color: #555;
+			    width: 100%;
+			    background-color: #fff;
+			    background-image: none;
+			    border: 1px solid #ccc;
+			    border-radius: 4px;
+			    -webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.075);
+			    box-shadow: inset 0 1px 1px rgba(0,0,0,.075);
+			}
+			#mc_embed_signup_scroll .button {
+				border-radius: 25px!important;
+			}
+			/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+			   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+		</style>
+		<div id="mc_embed_signup">
+		<form action="//twigghowto.us9.list-manage.com/subscribe/post?u=e02731bc69ed913d411b3f7e4&amp;id=84521ef699" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+		    <div id="mc_embed_signup_scroll">
+			
+		<div class="mc-field-group">
+			<label for="mce-EMAIL"><span class="asterisk">*</span>
+		</label>
+			<input type="email" value="" placeholder="Email Address" name="EMAIL" class="required email" id="mce-EMAIL">
+		</div>
+		<div class="submit-mce"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+		    </div>
+		<div id="mce-responses" class="clear">
+			<div class="response" id="mce-error-response" style="display:none"></div>
+			<div class="response" id="mce-success-response" style="display:none"></div>
+		</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+		<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_e02731bc69ed913d411b3f7e4_84521ef699" tabindex="-1" value=""></div>
+	
+		</form>
+		</div>
+		<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+		<!--End mc_embed_signup-->
+		</div>
+	</div>
+	<div class="subscribeLink col-xs-12 col-sm-6 col-md-6">
+		<h2 class="script">Get Involved</h2>
+		
+		<h4><a href="/become-a-contributor">Become a Contributor</a></h4>
+			<p>-- or --</p>
+		<h4><a href="/contactus#submithowto">Submit a How-to Request</a></h4>
+
+	</div>
+  	</div>
+  </div>
 <footer class="content-info" role="contentinfo">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-    <ul>
-    	<li><a href="/terms-and-conditions">Terms and Conditions</a></li>
-    	<li><a href="/contactus">Contact Us</a></li>
-    	<li><a href="http://www.gofundme.com/fampok" target="_blank"><img src="http://www.twigghowto.com/wp-content/uploads/2015/02/donate.png" width="75"></a></li>
-    </ul>
+  	<div class="row">
+  		<div class="col-md-10 col-md-offset-1">
+  		<div class="col-xs-12 col-sm-3 col-md-3 leftBorder">
+  			<img class="logo" src="http://www.twigghowto.com/wp-content/uploads/2015/03/twigg-logo.png">
+		    <?php dynamic_sidebar('sidebar-footer'); ?><br />
+		    <a href="http://www.gofundme.com/fampok" class="donateCta" target="_blank"><img src="http://www.twigghowto.com/wp-content/uploads/2015/02/donate.png"></a>
+		</div>
+		<div class="col-xs-12 col-sm-3 col-md-3 last">
+    		<?php get_template_part('templates/social'); ?>
+    		<?php get_search_form(); ?>
+    		
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-6">
+		    <ul class="mainLinks">
+		    	<li><a href="/about-us">About Us</a></li>
+		    	<li><a href="/behind-the-scenes">Behind the Scenes</a></li>
+		    	<li><a href="/stuff-we-love">Stuff We Love</a></li>
+		    	<li><a href="/events">Workshops</a></li>
+		    </ul>
+		    <ul>
+		    	<li><a href="/terms-and-conditions">Terms and Conditions</a></li>
+		    	<li><a href="/contact-us">Contact Us</a></li>
+		    </ul>
+		</div>
+		<!-- <div class="col-xs-12 col-sm-2 col-md-2">
+		    <ul>
+		    	
+		    	<li><a href="/contact-us">Contact Us</a></li>
+		    	<li><a href="/terms-and-conditions">Terms and Conditions</a></li>
+		    </ul>
+		</div> -->
+		</div>
+    </div>
     <p class="copyright">© Copyright TWIGG How-To <?php echo date('Y'); ?></p>
   </div>
 </footer>
